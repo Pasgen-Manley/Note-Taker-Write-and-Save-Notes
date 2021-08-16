ff8271e1487ecb5b9a052fe9b1988b6c811bfcb4
Pasgen-Manley <pasgen95@gmail.com>
2021-08-16 16:15:23 +0800

    notes.js finished

--- routes/notes.js
+++ routes/notes.js
@@ -4,3 +4,27 @@ const { notes } = require('../db/db.json');
 const fs = require('fs');
 
 const { makeNote, searchId, editNote, deleteNote } = require('../lib/utility');
+
+router.get('/notes', (req, res) => {
+  res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  if (!req.body.id) {
+    req.body.id = uuidv4();
+    createNewNote(req.body, notes);
+  } else {
+    editNote(req.body, notes);
+  } 
+
+  res.json(req.body);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const note = findById(req.params.id, notes);
+
+  removeNote(note, notes);
+  res.json();
+});
+
+module.exports = notes;
